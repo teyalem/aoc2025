84ae9bdf6e7e0a448c4ebdd8034e7c74f2a487bc
Hyeonung Baek <wiserlix@gmail.com>
2025-12-02 18:15:07 +0900

    Reformat day 01

--- bin/d01/main.ml
+++ bin/d01/main.ml
@@ -1,31 +1,31 @@
 let solve1 data =
-    List.fold_left (fun (d, c) (r, n) ->
-        let d = (d + (if r = 'L' then -1 else 1) * n) mod 100 in
-        d, (if d = 0 then c + 1 else c))
+  List.fold_left (fun (d, c) (r, n) ->
+      let d = (d + (if r = 'L' then -1 else 1) * n) mod 100 in
+      d, (if d = 0 then c + 1 else c))
     (50, 0)
     data
-    |> snd
+  |> snd
 
 let solve2 data =
-    let rec aux d c s n =
-        if n = 0 then d, c
-        else
-            let d = (d + s + 100) mod 100 in
-            let c = c + if d = 0 then 1 else 0 in
-            aux d c s (n - 1)
-    in
-    List.fold_left (fun (d, c) (r, n) ->
-        let s = if r = 'L' then -1 else 1 in
-        aux d c s n)
+  let rec aux d c s n =
+    if n = 0 then d, c
+    else
+      let d = (d + s + 100) mod 100 in
+      let c = c + if d = 0 then 1 else 0 in
+      aux d c s (n - 1)
+  in
+  List.fold_left (fun (d, c) (r, n) ->
+      let s = if r = 'L' then -1 else 1 in
+      aux d c s n)
     (50, 0)
     data
-    |> snd
+  |> snd
 
 let parse str =
-    Scanf.sscanf str "%c%d" (fun c d -> c, d)
+  Scanf.sscanf str "%c%d" (fun c d -> c, d)
 
 let () =
-    let data = open_in "input.txt" |> In_channel.input_lines |> List.map parse in
-    solve1 data |> print_int;
-    print_newline ();
-    solve2 data |> print_int
+  let data = open_in "input.txt" |> In_channel.input_lines |> List.map parse in
+  solve1 data |> print_int;
+  print_newline ();
+  solve2 data |> print_int
